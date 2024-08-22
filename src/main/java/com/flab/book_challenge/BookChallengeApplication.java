f7d2e97104d3d8b53fc3c1e8d86360b31e8e31ee
minsang-alt <mazinger97@naver.com>
2024-08-22 10:43:09 +0900

    feat: init project

---
+++ src/main/java/com/flab/book_challenge/BookChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.flab.book_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/book_challenge/BookChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.book_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
